1561ab80437d7200f5f8ce6d1c59f9a73657da89
Hermes-coding-kali <hermes.kali.music@gmail.com>
2025-09-18 02:42:56 -0700

    feat: Add new version of the site for testing

--- netlify/functions/create-checkout-session.js
+++ netlify/functions/create-checkout-session.js
@@ -50,11 +50,19 @@ exports.handler = async (event) => {
     const session = await stripe.checkout.sessions.create({
       mode: 'payment',
       line_items: lineItems,
-      // --- THIS IS THE MAGIC PART ---
       automatic_tax: { enabled: true },
-      // -----------------------------
+      
+      // --- ADD THIS SECTION ---
+      shipping_options: [
+        {
+          shipping_rate: 'shr_...', // <-- PASTE YOUR SHIPPING RATE ID HERE
+        },
+      ],
+      // ------------------------
+
       success_url: `${process.env.URL}/success`,
       cancel_url: `${process.env.URL}/cancel`,
+      // This line was already here and is required for shipping!
       shipping_address_collection: { allowed_countries: ['CA'] },
       allow_promotion_codes: true,
     });
