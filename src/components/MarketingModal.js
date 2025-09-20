e950c3d668a025fa85b14b2a325c90fe497ba0bc
Hermes-coding-kali <hermes.kali.music@gmail.com>
2025-09-20 15:22:58 -0700

    feat: Add new version of the site for testing

--- src/components/MarketingModal.js
+++ src/components/MarketingModal.js
@@ -1,10 +1,10 @@
 // src/components/MarketingModal.js
-
 import React from 'react';
 import { useModalStore } from '../stores/modalStore';
 import { urlFor } from '../sanityClient';
 import { PortableText } from '@portabletext/react';
 import CheckoutButton from './CheckoutButton';
+import Button from './Button'; // Import the Button component
 import './MarketingModal.css';
 
 function MarketingModal() {
@@ -20,18 +20,17 @@ function MarketingModal() {
           .quality(80)
           .url()
       : null;
+  
+  // Merge the base and override styles for the button
+  const buttonTokens = { ...modalData.buttonStyle, ...modalData.buttonOverride };
 
   return (
     <div className="modal-overlay" onClick={closeModal}>
       <div className="modal-card" onClick={(e) => e.stopPropagation()}>
         <button className="modal-close" onClick={closeModal}>&times;</button>
         
-        {/* --- NEW ELEMENT ORDER --- */}
-
-        {/* 1. Headline */}
         <h1 className="modal-headline">{modalData.headline}</h1>
 
-        {/* 2. Image */}
         {productImageUrl && (
           <div className="modal-image-container">
             <img
@@ -42,14 +41,16 @@ function MarketingModal() {
           </div>
         )}
 
-        {/* 3. Description (Body) */}
         <div className="modal-body">
           <PortableText value={modalData.body} />
         </div>
 
-        {/* 4. Call to Action Button */}
         <div className="modal-cta">
-          <CheckoutButton productId={modalData.featuredProduct._id} />
+          <CheckoutButton productId={modalData.featuredProduct._id}>
+            <Button styleTokens={buttonTokens}>
+              {modalData.ctaText || 'Buy Now'}
+            </Button>
+          </CheckoutButton>
         </div>
       </div>
     </div>
