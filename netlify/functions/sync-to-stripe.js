484f2ae20748fe385813c1fae102fd80b8a38efe
Hermes-coding-kali <hermes.kali.music@gmail.com>
2025-09-25 02:01:35 -0700

    Updated sync to stripe

--- netlify/functions/sync-to-stripe.js
+++ netlify/functions/sync-to-stripe.js
@@ -21,7 +21,7 @@ if (missing.length) {
 
 const sanityClient = createClient({
   projectId: process.env.SANITY_PROJECT_ID, 
-  dataset: process.env.SANITY_DATASET,
+  dataset: process.env.SANITY_DATASET, 
   useCdn: false,
   token: process.env.SANITY_API_TOKEN,
   apiVersion: process.env.SANITY_API_VERSION,
